e97561bacf6906f1666446dcbbb1f6dcab708b14
Joao Nunes <joao3001@hotmail.com>
2016-03-05 13:54:20 +0200

    Removed unused code

--- Sources/Future+Extensions.swift
+++ Sources/Future+Extensions.swift
@@ -53,47 +53,5 @@ extension Alamofire.Request {
 		
 		return promise.future
 	}
-	
-	/*
-	func responseJSONFuture() -> Future<AnyObject,TeslaError> {
-		
-		let promise = Promise<AnyObject, TeslaError>()
-		responseJSON { response in
-			
-			switch(response.result){
-			case .Success(let value):
-				print("Result: \(response.result.value!)")
-				promise.success(value)
-			case .Failure(let error):
-				print("Result: \(response.result.error!)")
-				promise.failure(TeslaError.NetworkError(error: error))
-			}
-		}
-		return promise.future
-	}*/
+
 }
-/*
-extension Mapper {
-	
-	func mapFuture(JSON: AnyObject?) -> Future<N,TeslaError> {
-		
-		let value = self.map(JSON)
-		if let value = value {
-			return Future<N,TeslaError>(value: value)
-		} else {
-			return Future<N,TeslaError>(error: .JSONParsingError(error: NSError(domain: "TeslaSwift", code: 0, userInfo: nil)))
-		}
-		
-	}
-	func mapArrayFuture(JSON: AnyObject?) -> Future<[N],TeslaError> {
-		
-		let value = self.mapArray(JSON)
-		if let value = value {
-			return Future<[N],TeslaError>(value: value)
-		} else {
-			return Future<[N],TeslaError>(error: .JSONParsingError(error: NSError(domain: "TeslaSwift", code: 0, userInfo: nil)))
-		}
-		
-	}
-	
-}*/
