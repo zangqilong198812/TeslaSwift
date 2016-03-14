e85901d2baa016dfae71653e3bbfd8650ec85312
João Nunes <joao3001@hotmail.com>
2016-03-14 18:40:30 +0200

    Refactored ChargeState to a new file

---
+++ Sources/Model/ChargeState.swift
@@ -0,0 +1,129 @@
+//
+//  ChargeState.swift
+//  TeslaSwift
+//
+//  Created by Joao Nunes on 14/03/16.
+//  Copyright © 2016 Joao Nunes. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+public class ChargeState:Mappable {
+	
+	enum ChargingState:String {
+		case Complete = "Complete"
+		case Charging = "Charging"
+		case Disconnected = "Disconnected"
+	}
+	
+	struct Distance {
+		private var value:Double
+		
+		init(miles:Double?) {
+			value = miles ?? 0.0
+		}
+		init(kms:Double) {
+			value = kms / 1.609344
+		}
+		
+		var miles:Double { return value }
+		var kms:Double { return value * 1.609344 }
+	}
+	
+	/**
+	Current state of the charging
+	*/
+	internal(set) var chargingState:ChargingState?
+	/**
+	Charge to max rate or standard
+	*/
+	internal(set) var chargeToMaxRange:Bool?
+	internal(set) var maxRangeChargeCounter:Int?
+	/**
+	Vehicle connected to supercharger?
+	*/
+	internal(set) var fastChargerPresent:Bool?
+	
+	/**
+	Rated Miles
+	*/
+	internal(set) var batteryRange:Distance?
+	/**
+	Range estimated from recent driving
+	*/
+	internal(set) var estimatedBatteryRange:Distance?
+	/**
+	Ideal Miles
+	*/
+	internal(set) var idealBatteryRange:Distance?
+	/**
+	Percentage of the battery
+	*/
+	internal(set) var batteryLevel:Int?
+	/**
+	Current flowing into the battery
+	*/
+	internal(set) var batteryCurrent:Double?
+	
+	internal(set) var chargeStartingRange:Double?
+	internal(set) var chargeStartingSOC:Double?
+	
+	/**
+	Voltage. Only has value while charging
+	*/
+	internal(set) var chargerVoltage:Int?
+	/**
+	Max current allowed by charger and adapter
+	*/
+	internal(set) var chargerPilotCurrent:Int?
+	/**
+	Current actually being drawn
+	*/
+	internal(set) var chargerActualCurrent:Int?
+	/**
+	KW of charger
+	*/
+	internal(set) var chargerPower:Int?
+	
+	
+	/**
+	Only valid while charging
+	*/
+	internal(set) var timeToFullCharge:Int?
+	/**
+	miles/hour while charging or -1 if not charging
+	*/
+	internal(set) var chargeRate:Double?
+	/**
+	Vehicle charging por is open?
+	*/
+	internal(set) var chargePortDoorOpen:Bool?
+	
+	
+	public required init?(_ map: Map) { }
+	
+	public func mapping(map: Map) {
+		
+		let distanceTransform = TransformOf<Distance, Double>(fromJSON: { Distance(miles: $0!) }, toJSON: {$0?.miles})
+		
+		chargingState			<- map["charging_state"]
+		chargeToMaxRange		<- map["charge_to_max_range"]
+		maxRangeChargeCounter	<- map["max_range_charge_counter"]
+		fastChargerPresent		<- map["fast_charger_present"]
+		batteryRange			<- (map["battery_range"], distanceTransform)
+		estimatedBatteryRange	<- (map["est_battery_range"], distanceTransform)
+		idealBatteryRange		<- (map["ideal_battery_range"], distanceTransform)
+		batteryLevel			<- map["battery_level"]
+		batteryCurrent			<- map["battery_current"]
+		chargeStartingRange		<- map["charge_starting_range"]
+		chargeStartingSOC		<- map["charge_starting_soc"]
+		chargerVoltage			<- map["charger_voltage"]
+		chargerPilotCurrent		<- map["charger_pilot_current"]
+		chargerActualCurrent	<- map["charger_actual_current"]
+		chargerPower			<- map["charger_power"]
+		timeToFullCharge		<- map["time_to_full_charge"]
+		chargeRate				<- map["charge_rate"]
+		chargePortDoorOpen		<- map["charge_port_door_open"]
+	}
+}
