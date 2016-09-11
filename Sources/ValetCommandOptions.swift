42bc1d27a3ea432f9473b227bf3ccd3def6ebd3d
Joao Nunes <joao3001@hotmail.com>
2016-09-11 20:32:57 +0300

    Changed project layout to support swift PM

---
+++ Sources/ValetCommandOptions.swift
@@ -0,0 +1,28 @@
+//
+//  ValetCommandOptions.swift
+//  TeslaSwift
+//
+//  Created by Joao Nunes on 12/04/16.
+//  Copyright © 2016 Joao Nunes. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+open class ValetCommandOptions: Mappable {
+	
+	open var on: Bool = false
+	open var password: String?
+	
+	init(valetActivated: Bool, pin: String?) {
+		on = valetActivated
+		password = pin
+	}
+	
+	required public init?(_ map: Map) { }
+	
+	open func mapping(_ map: Map) {
+		on			<- map["on"]
+		password	<- map["password"]
+	}
+}
