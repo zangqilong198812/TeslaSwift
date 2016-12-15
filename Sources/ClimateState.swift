617bcac26e5a0e2b53b85c8c874e79ef6486b8cb
Joao Nunes <joao3001@hotmail.com>
2016-12-15 19:33:44 +0200

    Moved ClimateState fields to match server ones

--- Sources/ClimateState.swift
+++ Sources/ClimateState.swift
@@ -25,35 +25,43 @@ open class ClimateState: Mappable {
 		public var fahrenheit: Double { return (value * 1.8) + 32.0 }
 	}
 	
-	open var insideTemperature: Temperature?
-	open var outsideTemperature: Temperature?
 	open var driverTemperatureSetting: Temperature?
-	open var passengerTemperatureSetting: Temperature?
-    open var maxAvailableTemperature: Temperature?
-    open var minAvailableTemperature: Temperature?
-    
-    open var isClimateOn: Bool?
-	open var isAutoConditioningOn: Bool?
-	open var isFrontDefrosterOn: Bool?
-	open var isRearDefrosterOn: Bool?
 	/*
 	* Fan speed 0-6 or nil
 	*/
 	open var fanStatus: Int?
-    open var smartPreconditioning: Int?
-    
+	
+	open var insideTemperature: Temperature?
+	
+	open var isAutoConditioningOn: Bool?
+	open var isClimateOn: Bool?
+	open var isFrontDefrosterOn: Bool?
+	open var isRearDefrosterOn: Bool?
+	
+	open var leftTemperatureDirection: Int?
+	
+	open var maxAvailableTemperature: Temperature?
+	open var minAvailableTemperature: Temperature?
+	
+	open var outsideTemperature: Temperature?
+	
+	open var passengerTemperatureSetting: Temperature?
+	
+	/*
+	* Temp directions 0 at least 583...
+	*/
+	open var rightTemperatureDirection: Int?
+	
+	
     open var seatHeaterLeft: Int?
-    open var seatHeaterRight: Int?
-    open var seatHeaterRearLeft: Int?
-    open var seatHeaterRearCenter: Int?
-    open var seatHeaterRearRight: Int?
-    open var seatHeaterRearLeftBack: Int?
-    open var seatHeaterRearRightBack: Int?
-    /*
-     * Temp directions 0 at least 583...
-     */
-    open var rightTemperatureDirection: Int?
-    open var leftTemperatureDirection: Int?
+	open var seatHeaterRearCenter: Int?
+	open var seatHeaterRearLeft: Int?
+	open var seatHeaterRearLeftBack: Int?
+	open var seatHeaterRearRight: Int?
+	open var seatHeaterRearRightBack: Int?
+	open var seatHeaterRight: Int?
+	
+	open var smartPreconditioning: Int?
 	
 	public required init?(map: Map) { }
 	
@@ -70,30 +78,38 @@ open class ClimateState: Mappable {
 			toJSON: {$0?.celsius}
 		)
 		
-		insideTemperature			<- (map["inside_temp"], temperatureTransform)
-		outsideTemperature			<- (map["outside_temp"], temperatureTransform)
 		driverTemperatureSetting	<- (map["driver_temp_setting"], temperatureTransform)
-		passengerTemperatureSetting <- (map["passenger_temp_setting"], temperatureTransform)
-        maxAvailableTemperature     <- (map["max_avail_temp"], temperatureTransform)
-        minAvailableTemperature     <- (map["min_avail_temp"], temperatureTransform)
-        isClimateOn                 <- map["is_climate_on"]
+		fanStatus					<- map["fan_status"]
+		
+		insideTemperature			<- (map["inside_temp"], temperatureTransform)
+		
 		isAutoConditioningOn		<- map["is_auto_conditioning_on"]
+		isClimateOn                 <- map["is_climate_on"]
 		isFrontDefrosterOn			<- map["is_front_defroster_on"]
 		isRearDefrosterOn			<- map["is_rear_defroster_on"]
-		fanStatus					<- map["fan_status"]
-        smartPreconditioning		<- map["smart_preconditioning"]
-        
+		
+		leftTemperatureDirection	<- map["left_temp_direction"]
+		
+		maxAvailableTemperature     <- (map["max_avail_temp"], temperatureTransform)
+		minAvailableTemperature     <- (map["min_avail_temp"], temperatureTransform)
+		
+		outsideTemperature			<- (map["outside_temp"], temperatureTransform)
+		
+		passengerTemperatureSetting <- (map["passenger_temp_setting"], temperatureTransform)
+		
+		rightTemperatureDirection	<- map["right_temp_direction"]
+		
+		
         seatHeaterLeft				<- map["seat_heater_left"]
-        seatHeaterRight				<- map["seat_heater_right"]
-        seatHeaterRearLeft			<- map["seat_heater_rear_left"]
-        seatHeaterRearCenter		<- map["seat_heater_rear_center"]
-        seatHeaterRearRight			<- map["seat_heater_rear_right"]
-        seatHeaterRearLeftBack		<- map["seat_heater_rear_left_back"]
-        seatHeaterRearRightBack		<- map["seat_heater_rear_right_back"]
-        
-        leftTemperatureDirection	<- map["left_temp_direction"]
-        rightTemperatureDirection	<- map["right_temp_direction"]
-        
+		seatHeaterRearCenter		<- map["seat_heater_rear_center"]
+		seatHeaterRearLeft			<- map["seat_heater_rear_left"]
+		seatHeaterRearLeftBack		<- map["seat_heater_rear_left_back"]
+		seatHeaterRearRight			<- map["seat_heater_rear_right"]
+		seatHeaterRearRightBack		<- map["seat_heater_rear_right_back"]
+		seatHeaterRight				<- map["seat_heater_right"]
+		
+		
+        smartPreconditioning		<- map["smart_preconditioning"]
         
 	}
 }
