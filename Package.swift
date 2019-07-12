e82f5ade56e41be7da5b922fcc48d73e36e3aa79
Joao Nunes <joao3001@hotmail.com>
2019-07-12 23:02:09 +0300

    Removed PromiseKit dependecy from SPM

--- Package.swift
+++ Package.swift
@@ -1,8 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "TeslaSwift",
-	dependencies: [
-		.Package(url: "https://github.com/mxcl/PromiseKit.git", versions: Version(6,0,0)..<Version(7,0,0))
-	]
+    name: "TeslaSwift"
 )
