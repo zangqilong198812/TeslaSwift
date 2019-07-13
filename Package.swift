9c199c13d10834ca1fc26303c722782f787458df
Joao Nunes <joao3001@hotmail.com>
2019-07-13 20:12:59 +0300

    Updated SPM Package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-    name: "TeslaSwift"
+    name: "TeslaSwift",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v10), .watchOS(.v3), .tvOS(.v10)
+    ],
+    products: [
+        .library(name: "TeslaSwift", targets: ["TeslaSwift"])
+    ],
+    targets: [
+        .target(name: "TeslaSwift"),
+        /*.testTarget(
+         name: "TeslaSwiftTests",
+         dependencies: ["TeslaSwift"],
+         path: "TeslaSwiftTests"
+         )*/
+    ]
 )
