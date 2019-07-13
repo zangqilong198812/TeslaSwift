33aabaea95318b6674e184556e8f65af9661d467
Joao Nunes <joao3001@hotmail.com>
2019-07-13 23:29:46 +0300

    Fixes for ios 13

--- TeslaSwiftDemo/LoginViewController.swift
+++ TeslaSwiftDemo/LoginViewController.swift
@@ -8,53 +8,60 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+extension Notification.Name {
+    
+    static let loginDone = Notification.Name("loginDone")
+}
 
-	@IBOutlet weak var emailTextField: UITextField!
-	@IBOutlet weak var passwordTextField: UITextField!
-	
-	
-	@IBOutlet weak var messageLabel: UILabel!
-	
-	@IBAction func loginAction(_ sender: AnyObject) {
-		
-		if let email = emailTextField.text,
-			let password = passwordTextField.text {
-			
-			UserDefaults.standard.set(email, forKey: "tesla.email")
-			UserDefaults.standard.set(password, forKey: "tesla.password")
-			UserDefaults.standard.synchronize()
-			
-			api.authenticate(email: email, password: password).done {
-				(token) -> Void in
-				
-				self.dismiss(animated: true, completion: nil)
-				
-				}.catch { (error) in
-					if case TeslaError.authenticationFailed =  error {
-						self.messageLabel.text = "Authentication failed"
-					} else {
-						self.messageLabel.text = "Error: \(error as NSError)"
-					}
-				}
-		} else {
-			messageLabel.text = "Please add your credentials"
-		}
-		
-	}
-	
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBAction func loginAction(_ sender: AnyObject) {
+        
+        if let email = emailTextField.text,
+            let password = passwordTextField.text {
+            
+            UserDefaults.standard.set(email, forKey: "tesla.email")
+            UserDefaults.standard.set(password, forKey: "tesla.password")
+            UserDefaults.standard.synchronize()
+            
+            api.authenticate(email: email, password: password).done {
+                (token) -> Void in
+                
+                NotificationCenter.default.post(name: Notification.Name.loginDone, object: nil)
+                
+                self.dismiss(animated: true, completion: nil)
+                
+            }.catch { (error) in
+                if case TeslaError.authenticationFailed =  error {
+                    self.messageLabel.text = "Authentication failed"
+                } else {
+                    self.messageLabel.text = "Error: \(error as NSError)"
+                }
+            }
+        } else {
+            messageLabel.text = "Please add your credentials"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-		
-		if let email = UserDefaults.standard.object(forKey: "tesla.email") as? String {
-			emailTextField.text = email
-		}
-		if let password = UserDefaults.standard.object(forKey: "tesla.password") as? String {
-			passwordTextField.text = password
-		}
-		
+        
+        
+        if let email = UserDefaults.standard.object(forKey: "tesla.email") as? String {
+            emailTextField.text = email
+        }
+        if let password = UserDefaults.standard.object(forKey: "tesla.password") as? String {
+            passwordTextField.text = password
+        }
+        
     }
 
-	
+
 }
