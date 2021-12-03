8f3321524bc0196ae5b4683bfbf38a059a60ec39
Alec O’Connor <aoconnor@mercury.io>
2021-12-03 14:17:04 -0500

    Remove silent error on Product, throw errors as it happens

--- Sources/TeslaSwift/Model/Product.swift
+++ Sources/TeslaSwift/Model/Product.swift
@@ -15,15 +15,15 @@ open class Product: Codable {
     open var isValidProduct: Bool { vehicle != nil || energySite != nil}
     
     required public init(from decoder: Decoder) throws {
-        let vehicleContainer = try? decoder.container(keyedBy: Vehicle.CodingKeys.self)
-        let energySiteContainer = try? decoder.container(keyedBy: EnergySite.CodingKeys.self)
+        let vehicleContainer = try decoder.container(keyedBy: Vehicle.CodingKeys.self)
+        let energySiteContainer = try decoder.container(keyedBy: EnergySite.CodingKeys.self)
         
-        if vehicleContainer?.contains(Vehicle.CodingKeys.vehicleID) ?? false {
-            self.vehicle = try? Vehicle(from: decoder)
+        if vehicleContainer.contains(Vehicle.CodingKeys.vehicleID) {
+            self.vehicle = try Vehicle(from: decoder)
             self.energySite = nil
-        } else if energySiteContainer?.contains(EnergySite.CodingKeys.energySiteID) ?? false {
+        } else if energySiteContainer.contains(EnergySite.CodingKeys.energySiteID) {
             self.vehicle = nil
-            self.energySite = try? EnergySite(from: decoder)
+            self.energySite = try EnergySite(from: decoder)
         } else {
             self.vehicle = nil
             self.energySite = nil
